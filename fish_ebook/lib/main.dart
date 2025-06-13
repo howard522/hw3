6a523fdad2a17f79d9908e752dd7ccd31fdc532a
howard522 <8754howard@gmail.com>
2025-06-13 22:09:40 +0800

    day 3

--- fish_ebook/lib/main.dart
+++ fish_ebook/lib/main.dart
@@ -1,122 +1,327 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:just_audio/just_audio.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => BookmarkModel(prefs),
+      child: const FishApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class BookmarkModel extends ChangeNotifier {
+  static const _key = 'favorites';
+  final SharedPreferences _prefs;
+  final Set<int> _favorites;
 
-  // This widget is the root of your application.
+  BookmarkModel(this._prefs)
+      : _favorites = (_prefs.getStringList(_key) ?? []).map(int.parse).toSet();
+
+  bool isFavorite(int id) => _favorites.contains(id);
+
+  void toggleFavorite(int id) {
+    if (_favorites.contains(id)) {
+      _favorites.remove(id);
+    } else {
+      _favorites.add(id);
+    }
+    _prefs.setStringList(
+      _key,
+      _favorites.map((i) => i.toString()).toList(),
+    );
+    notifyListeners();
+  }
+}
+
+class Fish {
+  final int id;
+  final String nameZh;
+  final String nameEn;
+  final String habitat;
+  final String description;
+  final String imageAsset;
+
+  const Fish({
+    required this.id,
+    required this.nameZh,
+    required this.nameEn,
+    required this.habitat,
+    required this.description,
+    required this.imageAsset,
+  });
+}
+
+const List<Fish> fishList = [
+  Fish(
+    id: 1,
+    nameZh: '小丑魚',
+    nameEn: 'Clownfish (Amphiprioninae)',
+    habitat: '熱帶珊瑚礁',
+    description: '以其鮮豔的橘白相間條紋聞名，與海葵共生。',
+    imageAsset: 'assets/images/clownfish.jfif',
+  ),
+  Fish(
+    id: 2,
+    nameZh: '藍刺尾魚',
+    nameEn: 'Blue Tang (Paracanthurus hepatus)',
+    habitat: '印度-太平洋珊瑚礁',
+    description: '又稱「多莉魚」，體型扁平，鮮藍色帶黑邊。',
+    imageAsset: 'assets/images/blue_tang.jfif',
+  ),
+  Fish(
+    id: 3,
+    nameZh: '海洋天使魚',
+    nameEn: 'Angelfish (Pomacanthidae)',
+    habitat: '熱帶淺海',
+    description: '體色絢麗，多樣化斑紋，常見於水族箱。',
+    imageAsset: 'assets/images/angelfish.jfif',
+  ),
+  Fish(
+    id: 4,
+    nameZh: '孔雀魚',
+    nameEn: 'Guppy (Poecilia reticulata)',
+    habitat: '淡水溫帶地區',
+    description: '小型淡水魚，繁殖力強，顏色繁多。',
+    imageAsset: 'assets/images/guppy.jpg',
+  ),
+  Fish(
+    id: 5,
+    nameZh: '鮭魚',
+    nameEn: 'Salmon (Salmonidae)',
+    habitat: '常見於餐桌',
+    description: '肉質鮮美，營養豐富。',
+    imageAsset: 'assets/images/salmon.jpg',
+  ),
+];
+
+class FishApp extends StatelessWidget {
+  const FishApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Fish eBook',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+// HomePage 改為 StatefulWidget 以管理音樂播放
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  late final AudioPlayer _audioPlayer;
+  bool _isPlaying = true;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer()..setLoopMode(LoopMode.all);
+    // 播放 assets 下的背景音樂，確保 pubspec.yaml 已註冊
+    _audioPlayer.setAsset('assets/audio/background.mp3').then((_) {
+      _audioPlayer.play();
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _togglePlayback() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_isPlaying) {
+        _audioPlayer.pause();
+      } else {
+        _audioPlayer.play();
+      }
+      _isPlaying = !_isPlaying;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fish eBook'),
+          actions: [
+            IconButton(
+              icon: Icon(_isPlaying ? Icons.music_off : Icons.music_note),
+              onPressed: _togglePlayback,
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: [Tab(text: 'List'), Tab(text: 'Grid')],
+          ),
+        ),
+        body: const TabBarView(
+          children: [FishListPage(), FishGridPage()],
+        ),
+      ),
+    );
+  }
+}
+
+class FishListPage extends StatelessWidget {
+  const FishListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: fishList.length,
+      itemBuilder: (context, index) {
+        final fish = fishList[index];
+        return Card(
+          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => FishDetailPage(fish: fish),
+                ),
+              );
+            },
+            child: ListTile(
+              leading: Image.asset(
+                fish.imageAsset,
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+              title: Text(fish.nameZh),
+              subtitle: Text(fish.habitat),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class FishGridPage extends StatelessWidget {
+  const FishGridPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: const EdgeInsets.all(12),
+      childAspectRatio: 3 / 4,
+      crossAxisSpacing: 12,
+      mainAxisSpacing: 12,
+      children: fishList.map((fish) {
+        return Card(
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => FishDetailPage(fish: fish),
+                ),
+              );
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Image.asset(
+                    fish.imageAsset,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    fish.nameZh,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
+
+class FishDetailPage extends StatelessWidget {
+  final Fish fish;
+  const FishDetailPage({Key? key, required this.fish}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bookmarks = context.watch<BookmarkModel>();
+    final isFav = bookmarks.isFavorite(fish.id);
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(fish.nameZh),
+        actions: [
+          IconButton(
+            icon: Icon(isFav ? Icons.bookmark : Icons.bookmark_border),
+            onPressed: () => bookmarks.toggleFavorite(fish.id),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              fish.imageAsset,
+              width: double.infinity,
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                elevation: 2,
+                margin: EdgeInsets.zero,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        fish.nameZh,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        fish.nameEn,
+                        style: const TextStyle(
+                            fontSize: 16, fontStyle: FontStyle.italic),
+                      ),
+                      const SizedBox(height: 8),
+                      Text('Habitat: ${fish.habitat}'),
+                      const SizedBox(height: 8),
+                      Text(fish.description),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
